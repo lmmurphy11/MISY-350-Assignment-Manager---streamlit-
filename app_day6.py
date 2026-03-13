af6c4aeca7be66d7a83ee8b90098a5953f00b2d5
Luke <lukemurphy@wifi-roaming-128-4-83-243.wifi.udel.edu>
2026-03-13 12:28:17 -0400

    Dashboard: needs update

--- app_day6.py
+++ app_day6.py
@@ -36,9 +36,186 @@ users = [
     }
 ]
 
+json_path = Path("user.json")
+if json_path.exists():
+    with json_path.open(json_path, "r") as f:
+        users = json.load(f)
 
-if st.session_state["role"] == "instructor":
-    st.markdown("Welcome! This is the instructor Dashboard")
+
+
+#load data
+assignments = [
+    {
+        "id": "HW1",
+        "title": "Intro to Database",
+        "description": "basics of database design",
+        "points": 100,
+        "type": "homework"
+    } ,
+    {
+        "id": "HW2",
+        "title": "Normalization",
+        "description": "normalizing",
+        "points": 100,
+        "type": "homework"
+    }
+]
+
+json_path = Path("assignments.json")
+
+#load the data from a json file
+if json_path.exists():
+    with json_path.open("r", encoding = "utf-8") as f:
+        assignments = json.load(f)
+
+
+
+
+if st.session_state["role"] == "Instructor":
+    if st.session_state["page"] == "home":
+        st.markdown("Welcome! This is the instructor Dashboard")
+        if st.button("Go to Dashboard", key = "dashboard_view_btn", type = "primary", use_container_width= True):
+            st.session_state["page"] = "dashboard"
+            st.rerun()
+    elif st.session_state["page"] == "dashboard":
+        st.markdown("Dashboard")
+
+tab1, tab2, tab3 = st.tabs(["View Assignments", "Add New Assignment", "Update an Assignment"])
+
+with tab1:
+
+    tab_option = st.radio("View/Search", ["View", "Search"], horizontal= True)
+    if tab_option == "View":
+        st.dataframe(assignments)
+    else:
+        titles = []
+        for assignment in assignments:
+            titles.append(assignment["title"])
+        
+        selected_title = st.selectbox("Select a title",titles, key ="selected_title")
+
+        selected_assignment = {}
+
+        for assignment in assignments:
+            if assignment["title"] == selected_title:
+                selected_assignment = assignment
+                break
+
+        st.divider()
+        selected_assignment = st.selectbox("Select Title",
+                                           options= assignments,
+                                           format_func= lambda x: f"{x['title']} [{x}]")
+
+
+        if selected_assignment:
+            with st.expander("Assignment Details", expanded = True):
+                st.markdown(f"### Title: {selected_assignment["title"]}")
+                st.markdown(f"Description: {selected_assignment["description"]}")
+                st.markdown(f"Type: **{selected_assignment["type"]}**")
+
+
+with tab2:
+    st.markdown("## Add New Assignment")
+    #st.markdown("### Add New Assignment")
+
+    title = st.text_input("Title")
+    description = st.text_area("Description", placeholder = "normalization is covered here", 
+                            help = "Here you are entering the assignmant details")
+    points = st.number_input("Points")
+
+    #assignment_type = st.text_input("Assignment Type")
+    assignment_type = st.radio("Type", ["Homework", "Lab"], horizontal = True)
+    assignment_type2 = st.selectbox("Type", ["Homework", "Lab", "other"])
+    if assignment_type2 == "other":
+        assignment_type2 = st.text_input("Type", placeholder = "Enter the Assignment Type")
+
+    due_date = st.date_input("Due Date")
+
+    btn_save = st.button("Save", width= "stretch", disabled = False)
+
+    import time
+    import json
+    from pathlib import Path
+
+    json_path = Path("assignments.json")
+
+
+    if btn_save:
+        if not title:
+            st.warning("Title needs to be provided!")
+        else:
+            with st.spinner("Assignment is being recorded...."):
+                time.sleep(5)
+
+                new_assignment_id = "HW" + str(next_assignment_id_number)
+                next_assignment_id_number += 1
+
+                assignments.append(
+                    {
+                        "id" : new_assignment_id,
+                        "title" : title,
+                        "description" : description,
+                        "points" : points,
+                        "type" : assignment_type
+                    }
+                )
+
+                with json_path.open("w", encoding= "utf-8") as f:
+                    json.dump(assignments, f)
+
+                st.success("New Assignment is recorded!")
+                st.info("This is a new assignment")
+                time.sleep(4)
+                #st.dataframe(assignments)
+                st.rerun()
+
+
+with tab3:
+    st.markdown("## Update an Assignment")
+    titles = []
+
+    for assignment in assignments:
+        titles.append(assignment["title"])
+
+    
+    selected_item = st.selectbox("Select an assignment", titles, key ="selected_title_edit")
+
+    assignment_edit = {}
+    for assignment in assignments:
+        if assignment['title'] == selected_item:
+            assignment_edit = assignment
+            break
+
+    if assignment_edit:   
+        edit_title = st.text_input("Title", key= f"edit_title_{assignment_edit['id']}", 
+                                   value= assignment_edit['title'])
+        edit_description = st.text_area("Description", key= "edit_description", 
+                                        value=assignment_edit['description'])
+        
+        type_options = ["Homework", "Lab"]
+        selected_index = type_options.index(assignment_edit["type"])
+
+        edit_type = st.radio("Type", type_options, 
+                            key = f"edit_assignment{assignment_edit['id']}",
+                            index = selected_index)
+                            
+
+    
+
+    btn_update = st.button("Update", key ="update_button", type= "secondary", use_container_width= True)
+    if btn_update:
+        with st.spinner("Updating..."):
+            time.sleep(5)
+            assignment_edit['title'] = edit_title
+            assignment_edit['description'] = edit_description
+
+            with json_path.open("w", encoding = "utf-8") as f:
+                json.dump(assignments, f)
+
+            st.success("Assignment is Updated!")
+            time.sleep(5)
+            st.rerun()
+
 
 elif st.session_state['role'] == "Admin":
     st.markdown("Welcome! This is the Admin Dashboard")
@@ -79,6 +256,9 @@ else:
                     st.session_state["logged_in"] = True
                     st.session_state["user"] = found_user
                     st.session_state["role"] = found_user["role"]
+                    st.session_state["page"] = "home"
+
+
                     time.sleep(2)
                     st.rerun()
                 else:
@@ -92,7 +272,7 @@ else:
         new_email = st.text_input("Email Address", key="email_register")
         new_password = st.text_input("Password", type="password", key="password_edit")
         
-        if st.button("Create Account"):
+        if st.button("Create Account", key = "register_btn"):
             with st.spinner("Creating account..."):
                 time.sleep(2) # Fake backend delay
                 # ... (Assume validation logic here) ...
@@ -102,7 +282,10 @@ else:
                     "password": new_password,
                     "role": "Instructor"
                 })
-                
+
+                with open(json_path, "w") as f:
+                    json.dump(users, f)
+
                 st.success("Account created!")
                 st.rerun()
 
