978436ea090c8a9dbce4750318e71222030f1c85
Luke <lukemurphy@wifi-roaming-128-4-95-20.wifi.udel.edu>
2026-04-15 12:26:59 -0400

    4/15

--- smart_kiosk/services/kiosk_services.py
+++ smart_kiosk/services/kiosk_services.py
@@ -1,22 +1,18 @@
 import uuid
+from typing import Optional, List, Dict
 
-
-#Query 1: Place a new order for an item and quantity
-
-#find orders placed for an inventory item using item id
-
-#Step 2: find how many orders placed for the item using item id
-
-#find item information in inventory
-
-def place_order(inventory: list, orders: list, item_id: str, quantity: int):
-    #find ite in the inventory
+def place_order(inventory: list, orders: list, item_id: str, quantity: int) -> Optional[Dict]:
+    #find item in the inventory
     item = find_inventory_item_by_item_id(inventory, item_id)
     #if it exists -> 
     if item:
         if item['stock'] >= quantity:
             item['stock'] = item['stock'] - quantity
             total_cost = item['unit_price'] * quantity
+
+            #if the stock is > the quantity asked
+            #reduce the inventory
+            #then place the new order
             new_order ={
                 "order_id": str(uuid.uuid4()),
                 "item_id": item_id,
@@ -27,16 +23,13 @@ def place_order(inventory: list, orders: list, item_id: str, quantity: int):
             orders.append(new_order)
             return new_order
 
-    #if the stock is > the quantity asked
-        #reduce the inventory
-        #then place the new order
-    pass
 
-def find_inventory_item_by_item_id(inventory: list, item_id: str):
+
+def find_inventory_item_by_item_id(inventory: list, item_id: str) -> Optional[Dict]:
     for item in inventory:
-        if item['id'] == item_id:
+        if item['item_id'] == item_id:
             return item
-        return []
+        return None
     
 
 def update_order_status():
